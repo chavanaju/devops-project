8804ddeaea6b5be73282d43876a59c30a9ccdf28
Ubuntu <ubuntu@ip-10-0-0-126.ec2.internal>
2024-06-23 10:39:12 +0000

    app.py updated

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def helloworld():
+    if request.method == 'GET':
+        name = request.args.get('name', 'World')
+        data = {"data": f"Hello {name}"}
+        return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
